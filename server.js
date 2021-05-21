e3d5eca513d3d3ea141b0691af4160683b9cbff2
Joshua Abeyta <mentaltemple@gmail.com>
2021-05-21 16:22:54 -0600

    linked db.json file to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const noteData = require("./db/db.json");
 
 //sets up the Express app
 const app = express();
@@ -12,12 +13,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// Basic route that sends the user first to the correct page
+// Basic route that first sends the user to the correct page
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "notes.html"))
 );
 
+//display notes
+app.get("/notes", (req, res) => res.json(noteData));
 //starts the server to begin listening
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
