c013003c63955a209e61ff8071f8f29455dccc6b
Joshua Abeyta <mentaltemple@gmail.com>
2021-05-21 15:40:38 -0600

    added basic routes in server.js

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
+//pulls in dependencies
 const express = require("express");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+const path = require("path");
+const fs = require("fs");
 
+//sets up the Express app
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//sets up the Express app to handle data parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
 
+// Basic route that sends the user first to the correct page
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "notes.html"))
+);
+
+//starts the server to begin listening
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
